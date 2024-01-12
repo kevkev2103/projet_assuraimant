f75b6872a97144f0e0dff7859861a922101ce367
kevin <kevin.pnathan@gmail.com>
2024-01-12 21:27:59 +0100

    maj

--- fonction_bmi.py
+++ fonction_bmi.py
@@ -11,10 +11,3 @@ def categorisation_numerique(df, bins, labels, categorie):
     df = df.drop(['nouvelle_categorie'], axis=1)
     return df
 
-df = pd.read_csv('data/df_clean.csv')
-
-df = categorisation_numerique(df, [0, 18.5, 24.9, 28.5, 35, 40, float('inf')],
-                               ['maigreur', 'sous_poids', 'poids_normal', 'surpoids', 'obésité', 'obésité_morbide'],
-                               'bmi')
-
-print(df)
