2706c083625e0d081a71d402dfc4ba8dba4aa5c4
Maxime KUIL <maximekuil@gmail.com>
2024-01-12 15:32:01 +0100

    test shape graph

--- app/main.py
+++ app/main.py
@@ -8,8 +8,8 @@ import pandas as pd
 #   catégorielle : ['region'] (southwest, southeast, northeast, northwest)
 #   label : ['smoker'] (yes, no)
 
-model_path = 'data/model_cv.pickle'
-#model_path = 'data/model_grid.pickle'
+#model_path = 'data/model_cv.pickle'
+model_path = 'data/model_grid.pickle'
 
 m = pickle.load(open(model_path, 'rb'))
 model = m['model']
@@ -29,17 +29,21 @@ regionVal = {
     'Sud-ouest': 'southwest'
 }
 
-columns = ['smoker', 'age', 'bmi', 'children', 'region']
-
 with st.form("my_form"):
     st.title("Calculez votre prime d'assurance")
     
+    sex_val = st.radio(
+            "Sexe",
+            [key for key, val in sexVal.items()],
+            key="sex", horizontal=True)
     smoker_val = st.radio(
             "Fumeur",
             [key for key, val in smokerVal.items()],
             key="smoker", horizontal=True)
+    poids_val = st.number_input('Poids', step=1)
+    taille_val = st.number_input('Taille en cm', step=1)
     age_val = st.number_input('Age', min_value=18, step=1)
-    bmi_val = st.number_input('IMC', min_value=15., max_value=65., step=.1)
+    #bmi_val = st.number_input('IMC', min_value=15., max_value=65., step=.1)
     children_val = st.number_input('Enfants', min_value=0, step=1)
     
     region_val = st.selectbox('Région',
@@ -48,24 +52,20 @@ with st.form("my_form"):
     
     submitted = st.form_submit_button("Prédire !")
     if submitted:
-        print([
-                    [smokerVal[smoker_val]],
-                    [age_val],
-                    [bmi_val],
-                    [children_val],
-                    [regionVal[region_val]]
-                ])
+        t = taille_val / 100
+        bmi = poids_val / t**t
+        columns = ['sex', 'smoker', 'age', 'bmi', 'children', 'region']
+
         df =  pd.DataFrame([[
+                        sexVal[sex_val],
                         smokerVal[smoker_val],
                         age_val,
-                        bmi_val,
+                        bmi,
                         children_val,
                         regionVal[region_val]
                     ]], columns=columns)
         
-        df_process = preprocessor.transform(df)
-        print(df)
-        print(df_process)
-        predict = model.predict(df_process)
+        print(df.head())
+        predict = model.predict(df)
         
         st.write(f'{round(predict[0], 2)}€')
